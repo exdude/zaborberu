3c70924da41c8e86a08c6fde35c71d2c57ef95ca
Rinat <exd4work@gmail.com>
2021-06-22 09:15:40 +0500

    correct styles

--- server/middleware/domianRedirect/index.js
+++ server/middleware/domianRedirect/index.js
@@ -1,10 +1,14 @@
 const rtl = require('../rtl')
 module.exports = (req, res, next) => {
-    const data = JSON.parse(req.cookies.data)
-    if (req.get('host').split('.').length >= 3) {
-        next()
+    if (req.cookies.data) {
+        const data = JSON.parse(req.cookies.data)
+        if (req.get('host').split('.').length >= 3) {
+            next()
+        } else {
+            const city = rtl(data.name).toLowerCase()
+            res.redirect(`https://${city}.${req.hostname}${req.originalUrl}`);
+        }
     } else {
-        const city = rtl(data.name).toLowerCase()
-        res.redirect(`https://${city}.${req.hostname}${req.originalUrl}`);
+        next()
     }
 }
